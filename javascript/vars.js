e8cb7f765a5887342132ff13034d7f2701da8471
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2022-11-06 20:19:53 +0000

    redesign 2.0

--- javascript/vars.js
+++ javascript/vars.js
@@ -1,57 +1,37 @@
 //Houses
-var HouseName = ['Griffindor', 'Ravenclaw', 'Hufflepuff', 'Slitherin'];
-var HousePoints = ['10', '20', '25', '-10'];
+var CollegeName = [];
+var CollegePoints = [];
 
 // Teachers Table
-var TeacherHouse = ['Griffindor', 'Ravenclaw', 'Hufflepuff', 'Ravenclaw'];
-var TeacherName = ['Mrs Spandex', 'Mr bennett', 'Mr Titan', 'Dr Hooters'];
-
+var TeacherHouse = [];
+var TeacherName = [];
+var TeacherRank = [];
 
 // Studnets Table
-var StudentHouse = ['Griffindor', 'Hufflepuff', 'Ravenclaw', 'Hufflepuff'];
-var StudentName = ['Student 1', 'Student 2', 'Student 3', 'Student 4'];
-var StudentPlayer = ['Yes', 'No', 'No', 'Yes'];
-var StudentRace = ['Human', 'Gith', 'Loxodon', 'Goblin'];
-var StudentGender = ['Male', 'Female', 'Male2', 'Female2']
-var StudentAge = ['25', '22', '100', '4'];
-var StudentDetail4 = ['', '', '', ''];
-var StudentDetail5 = ['', '', '', ''];
-var StudentDetail6 = ['', '', '', ''];
+var StudentHouse = [];
+var StudentName = [];
+var StudentPlayer = [];
+var StudentRace = [];
+var StudentGender = []
+var StudentAge = [];
+var StudentDetail4 = [];
+var StudentDetail5 = [];
+var StudentDetail6 = [];
 
 
 // Relationships
-var RelMorgan = ['10', '10', '10', '10'];
-var RelSami = ['8', '8', '8', '8'];
-var RelTimo = ['5', '7', '8', '4'];
-var RelMart = ['1', '4', '6', '7'];
-var RelWill = ['7', '4', '6', '3'];
-var RelLover = ['Student 2 & Student 4', 'Student 1', '', 'Student 1'];
-var RelRival = ['Student 3', 'Student4', 'Student 1', ''];
+var RelMorgan = [];
+var RelSami = [];
+var RelTimo = [];
+var RelMart = [];
+var RelWill = [];
+var RelLover = [];
+var RelRival = [];
 
 // Report Card
-var ExamPerson = ['Student 1', 'Student 1', 'Student 1', 'Student 1', 'Student 1', 'Student 1', 'Student 1', 'Student 1', 'Student 1', 'Student 1',
-                  'Student 2', 'Student 2', 'Student 2', 'Student 2', 'Student 2', 'Student 2', 'Student 2', 'Student 2', 'Student 2', 'Student 2',
-                  'Student 3', 'Student 3', 'Student 3', 'Student 3', 'Student 3', 'Student 3', 'Student 3', 'Student 3', 'Student 3', 'Student 3',
-                  'Student 4', 'Student 4', 'Student 4', 'Student 4', 'Student 4', 'Student 4', 'Student 4', 'Student 4', 'Student 4', 'Student 4'];
-
-var ExamScore = ['Fail', 'Pass', 'Aced', 'Pass', 'Aced', 'Fail', 'Fail', 'Fail', 'Fail', 'Aced',
-                 'Fail', 'Pass', 'Aced', 'Pass', 'Aced', 'Fail', 'Fail', 'Fail', 'Fail', 'Aced',
-                 'Fail', 'Pass', 'Aced', 'Pass', 'Aced', 'Fail', 'Fail', 'Fail', 'Fail', 'Aced',
-                 'Fail', 'Pass', 'Aced', 'Pass', 'Aced', 'Fail', 'Fail', 'Fail', 'Fail', 'Aced'];
-
-var ExamStudy = ['Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No', 'No',
-                 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No', 'No',
-                 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No', 'No',
-                 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'Yes', 'No', 'No'];
-
-var ExamExtraCreit = ['Yes', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No',
-                      'Yes', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No',
-                      'Yes', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No',
-                      'Yes', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No', 'No'];
-
-var ExamCheated = ['No', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No', 'No', 'Yes',
-                   'No', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No', 'No', 'Yes',
-                   'No', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No', 'No', 'Yes',
-                   'No', 'No', 'Yes', 'No', 'Yes', 'No', 'No', 'No', 'No', 'Yes'];
-
-var ExamNumber = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+var ExamPerson = [];
+var ExamScore = [];
+var ExamStudy = [];
+var ExamExtraCreit = [];
+var ExamCheated = [];
+var ExamNumber = [];
