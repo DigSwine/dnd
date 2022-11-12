13cd00ac276d52f4a208ba5fdb5927018a241bff
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2022-11-12 18:59:00 +0000

    preload

--- javascript/preload.js
+++ javascript/preload.js
@@ -1,22 +1,30 @@
 $(document).ready(function () {
-    preload();
+    preload(pageid);
 });
 
 // Pre load images 
-function preload() {
-    var images = [
-        "blackmarket.png",
-        "MtG_background.jpg",
-        "strix_logo.png",
-        "strix_map.png",
-        "collegebackground.jpg",
-        "maps/central_map.png",
-        "maps/lorehold_map.png",
-        "maps/prismari_map.png",
-        "maps/quandrix_map.png",
-        "maps/silverquill_map.png",
-        "maps/witherbloom_map.png"
-    ];
+function preload(page) {
+    var images = [];
+
+    if (page == 'index') {
+        images = [
+            "blackmarket.png",
+            "MtG_background.jpg",
+            "strix_logo.png",
+            "strix_map.png",
+            "collegebackground.jpg"
+        ];
+    }
+    if (page == 'maps') {
+        images = [
+            "maps/central_map.png",
+            "maps/lorehold_map.png",
+            "maps/prismari_map.png",
+            "maps/quandrix_map.png",
+            "maps/silverquill_map.png",
+            "maps/witherbloom_map.png"
+        ];
+    }
     for (var i = 0; i < arguments.length; i++) {
         console.log(images[i]);
         images[i] = '../assets/' + images[i];
