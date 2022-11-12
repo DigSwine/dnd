e3b852537ff9c63f9f97b33fbc054ffa188727fd
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2022-11-12 17:49:12 +0000

    preload maps

--- javascript/preload.js
+++ javascript/preload.js
@@ -8,7 +8,14 @@ function preload() {
         "blackmarket.png",
         "MtG_background.jpg",
         "strix_logo.png",
-        "strix_map.png"
+        "strix_map.png",
+        "collegebackground.jpg",
+        "maps/central_map.png",
+        "maps/lorehold_map.png",
+        "maps/prismari_map.png",
+        "maps/quandrix_map.png",
+        "maps/silverquill_map.png",
+        "maps/witherbloom_map.png"
     ];
     for (var i = 0; i < arguments.length; i++) {
         console.log(images[i]);
