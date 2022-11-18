2908ecfdd08616ab7765aaf779128513b02fc526
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2022-11-18 13:54:47 +0000

    college details

--- javascript/maps.js
+++ javascript/maps.js
@@ -9,7 +9,7 @@ function collegedetails(college) {
         src = 'silverquill.png';
         collegename = 'Silverquill';
         collegequote = 'Sharp style, sharper wit.';
-        collegedescription = '';
+        collegedescription = "Right north of the central campus lays a region that looks very much like an extension of it. Silverquill's campus is the most urban of all five colleges, with buildings of stately elegance spread across it.";
         map = 'silverquill_map.png';
         textref = '_silverquill.html';
     }
@@ -17,7 +17,7 @@ function collegedetails(college) {
         src = 'prismari.png';
         collegename = 'Prismari';
         collegequote = 'Express yourself, with the elements!';
-        collegedescription = '';
+        collegedescription = "To the southwest of the Central campus there is a region of jagged rocks, rugged spires, and intense geothermal activity. This is where the Prismari College resides.";
         map = 'prismari_map.png';
         textref = '_prismari.html';
     }
@@ -25,7 +25,7 @@ function collegedetails(college) {
         src = 'lorehold.png';
         collegename = 'Lorehold';
         collegequote = 'Leave no stone unturned!';
-        collegedescription = '';
+        collegedescription = "The Lorehold Campus is situated northwest of the central campus in a region of stark vertical relief. It's a desert like environment with plenty of mountain mesas, as well as a great chasm filled with ruins from an ancient settlement.";
         map = 'lorehold_map.png';
         textref = '_lorehold.html';
     }
@@ -33,7 +33,7 @@ function collegedetails(college) {
         src = 'quandrix.png';
         collegename = 'Quandrix';
         collegequote = 'Math is magic.';
-        collegedescription = '';
+        collegedescription = "To the northeast of the Central Campus there lays a coastline, which is filled by abundant rivers and streams as well as lush woodland. This is where the Quandrix College commits their study.";
         map = 'quandrix_map.png';
         textref = '_quandrix.html';
     }
@@ -41,7 +41,7 @@ function collegedetails(college) {
         src = 'witherbloom.png';
         collegename = 'Witherbloom';
         collegequote = 'Get your hands dirty.';
-        collegedescription = '';
+        collegedescription = "Southeast of the central campus lays a wide bayou, in which the Witherbloom campus is tucked away.";
         map = 'witherbloom_map.png';
         textref = '_witherbloom.html';
     }
