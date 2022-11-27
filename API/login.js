6309c16370273353cd8e2353000b70511a6df417
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2022-11-27 15:14:18 +0000

    Login works

--- API/login.js
+++ API/login.js
@@ -1,40 +1,35 @@
-function login(clicked, us, ps) {
-    const { createClient } = supabase;
+$("button").click(function () {
+    
+    login($("#user").val(), $("#pass").val())
 
+});
+
+function login(us, ps) {
+    const { createClient } = supabase;
     var anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkY2Nhem1uemlwcGJsbnZ2cWRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzczNDkyOCwiZXhwIjoxOTgzMzEwOTI4fQ.79swd9Gvw45ziqMzXrIxPSnQU67K6swiVnSJAzujLqA';
     var url = 'https://pdccazmnzippblnvvqdp.supabase.co';
     const _supabase = createClient(url, anon);
 
-    if (ps != "newps" && clicked == 'btn') {
         _supabase.from('_tblLogin').select('*').match({ log_use: us, log_pas: ps }).then(response => {
-            extractdata(clicked, response.data);
+            if (response.data) {
+                // alert(response.data);
+                extractdata(response.data);
+            } else {
+                alert("Failed to log in please try again");
+            }            
         })
-    } else if (ps != "newps" && clicked == 'bm') {
-        _supabase.from('_tblLogin').select('*').match({ log_use: us, log_pas: ps }).then(response => {
-            extractdata(clicked, response.data);
-        })
-    } else {
-        console.log(clicked + " " + ps);
-        alert(requestNewPassword());
-    }
 }
-function extractdata(type, array) {
+
+function extractdata(array) {
     for (var x = 0; x < array.length; x++) {
         var data = array[x];
         var per = data['log_per'];
-
-        if (type == 'btn') {
-            if (per == 'Stu') {
-                sessionStorage.setItem("Data", JSON.stringify(data));
-                window.open('studentview.html', '_self');
-            }
-            if (per == 'Tch') {
-                window.open('teacherview.html', '_self');
-            }
+        if (per == 'Stu') {
+            sessionStorage.setItem("Data", JSON.stringify(data['student_id']));
+            window.open('studentview.html', '_self');
         }
-        if (type == 'bm') {
-            sessionStorage.setItem("Data", JSON.stringify(data));
-            window.open('blackmarket.html', '_self');
+        if (per == 'Tch') {
+            window.open('teacherview.html', '_self');
         }
     }
 }
