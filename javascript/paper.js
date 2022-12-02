46476d06a9586176e94af14bd89d8bd86eab92dd
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2022-12-02 13:23:32 +0000

    lock map btns

---
+++ javascript/paper.js
@@ -0,0 +1,12 @@
+class pagecontent {
+    constructor(page, location, title, author, content, img) {
+        this.page = page;
+        this.location = location;
+
+        this.title = title;
+        this.author = author;
+        this.content = content;
+
+        this.img = img;
+    }
+}
