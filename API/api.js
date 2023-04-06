16ea57203a5034640b939f280f4fd391669471cd
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2023-04-06 12:47:05 +0100

    set

--- API/api.js
+++ API/api.js
@@ -1,32 +1,4 @@
-//Database Scripts
-function getAll(toget) {
-    const { createClient } = supabase;
-
-    var anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkY2Nhem1uemlwcGJsbnZ2cWRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzczNDkyOCwiZXhwIjoxOTgzMzEwOTI4fQ.79swd9Gvw45ziqMzXrIxPSnQU67K6swiVnSJAzujLqA';
-    var url = 'https://pdccazmnzippblnvvqdp.supabase.co';
-    const _supabase = createClient(url, anon);
-
-    if (toget == "college" || toget == "all") {
-        // College Data
-        _supabase.from('view_allcolleges').select('*').then(response => {
-            // extractdata('colleges', response.data);
-        })
-    }
-
-    if (toget == "student" || toget == "all") {
-        // Student Data
-        _supabase.from('view_allstudents').select('*').then(response => {
-            // extractdata('students', response.data);
-        })
-    }
-
-    if (toget == "collegemembers" || toget == "all") {
-        // Link Students to Colleges
-        _supabase.from('view_student_college').select('*').then(response => {
-            // extractdata('collegemembers', response.data);
-        })
-    }
-
-    // Exam Data
-}
-
+const { createClient } = supabase;
+var anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkY2Nhem1uemlwcGJsbnZ2cWRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzczNDkyOCwiZXhwIjoxOTgzMzEwOTI4fQ.79swd9Gvw45ziqMzXrIxPSnQU67K6swiVnSJAzujLqA';
+var url = 'https://pdccazmnzippblnvvqdp.supabase.co';
+const _supabase = createClient(url, anon);
--- API/login.js
+++ API/login.js
@@ -1,18 +1,10 @@
 $("button").click(function () {
-    
     login($("#user").val(), $("#pass").val())
-
 });
 
 function login(us, ps) {
-    const { createClient } = supabase;
-    var anon = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkY2Nhem1uemlwcGJsbnZ2cWRwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2NzczNDkyOCwiZXhwIjoxOTgzMzEwOTI4fQ.79swd9Gvw45ziqMzXrIxPSnQU67K6swiVnSJAzujLqA';
-    var url = 'https://pdccazmnzippblnvvqdp.supabase.co';
-    const _supabase = createClient(url, anon);
-
         _supabase.from('_tblLogin').select('*').match({ log_use: us, log_pas: ps }).then(response => {
             if (response.data) {
-                // alert(response.data);
                 extractdata(response.data);
             } else {
                 alert("Failed to log in please try again");
