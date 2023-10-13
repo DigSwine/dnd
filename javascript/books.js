863e25a3fb9ab9dd98318473a3cc499d605ea2be
Morgan <morgan.wilson-slider@students.plymouth.ac.uk>
2023-10-13 10:55:47 +0100

    books

---
+++ javascript/books.js
@@ -0,0 +1,98 @@
+var books = {}
+
+books.races = {}
+books.classes = {}
+books.backgrounds = {}
+
+// Races
+books.races.name = [
+    "Ratling",
+    "Dhampirica Hybrids",
+    "Fet'Ri",
+    "Runescale",
+    "Kitsune",
+    "Cecaelia",
+    "Panserbjorn"
+];
+books.races.author = [
+    "KR",
+    "SM",
+    "DMH",
+    "LTM",
+    "GW",
+    "RH",
+    "HW"
+];
+books.races.url = [
+    "https://homebrewery.naturalcrit.com/share/300F09N27W_6",
+    "https://homebrewery.naturalcrit.com/share/6gt0kTEMji7N",
+    "https://www.gmbinder.com/share/-MMvBiGPfZvHkW9AT2Oq",
+    "https://www.gmbinder.com/share/-N78Y1evACOAeovaGoDl",
+    "https://homebrewery.naturalcrit.com/share/5G7vZcmiFzEa",
+    "https://dnd-5e-homebrew.tumblr.com/post/149175265286/cacaelia-race-by-absurdisan/amp",
+    "https://drive.google.com/file/d/15aNw4RcWnHz3udZ38WskSsL8XKLb95NR/view"
+    
+];
+books.races.img = [
+    "https://i.pinimg.com/564x/fd/b4/c4/fdb4c409efdc7bf2646f672277534d56.jpg",
+    "https://i.pinimg.com/564x/3b/bd/44/3bbd44419e9ab1826dc0569c2dd7a743.jpg",
+    "https://cdnb.artstation.com/p/assets/images/images/017/671/207/large/astri-lohne-zevrx-final.jpg?1556894528",
+    "https://64.media.tumblr.com/4a80d56233100444dfaf79284a5c59b6/tumblr_inline_ppq48ty3zf1rkapbx_1280.png",
+    "https://www.scan.co.uk/images/homepages/progaming/guides_lol_ahri.jpg",
+    "https://cdnb.artstation.com/p/assets/images/images/022/652/683/large/dusan-markovic-9a-lemuria.jpg?1576195920",
+    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/827d0cfa-922f-4029-9c4a-2db7b0145d15/dbz8ane-fec457f5-d09c-4b69-8123-828a136feb9d.jpg/v1/fill/w_1210,h_660,q_70,strp/war_bear_by_dongjunlu_dbz8ane-pre.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9Njk5IiwicGF0aCI6IlwvZlwvODI3ZDBjZmEtOTIyZi00MDI5LTljNGEtMmRiN2IwMTQ1ZDE1XC9kYno4YW5lLWZlYzQ1N2Y1LWQwOWMtNGI2OS04MTIzLTgyOGExMzZmZWI5ZC5qcGciLCJ3aWR0aCI6Ijw9MTI4MCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl19.aAKbXbY6gMkiqvhdk8cMfNkCtzhvDYStni57Pl2YOj0"
+];
+// Classes
+books.classes.name = [
+    "Duplicator",
+    "Blink Mage",
+    "Mage Hunter",
+    "Marksman",
+    "Warlock Patron: <br> The Succubus",
+    "The Doctor"
+];
+books.classes.author = [
+    "DS",
+    "IA",
+    "VD",
+    "S",
+    "VD",
+    "SM"
+];
+books.classes.url = [
+    "https://homebrewery.naturalcrit.com/share/RcPEnSaJ5nrb",
+    "https://homebrewery.naturalcrit.com/share/EGRM4xOrtHQP",
+    "https://homebrewery.naturalcrit.com/share/UJKvsxEaGLuD",
+    "https://www.gmbinder.com/share/-MssFRJW4q20toN8higH",
+    "https://homebrewery.naturalcrit.com/share/VnHNpsYX-SGm1",
+    "https://homebrewery.naturalcrit.com/share/P1a4HjHAsPLG"
+];
+books.classes.img = [
+    "https://steamuserimages-a.akamaihd.net/ugc/943958354987860688/A8D0883AD68233784FB7274BB347E278FEA85E8D/?imw=5000",
+    "https://i.imgur.com/Nb5DB9d.png",
+    "https://i.pinimg.com/originals/48/41/e8/4841e8adab4f2257d6ad89f296d53fd2.jpg",
+    "https://cdnb.artstation.com/p/assets/images/images/000/661/881/large/james-paick-20.jpg?1430174599",
+    "https://i.pinimg.com/originals/35/41/eb/3541eb3b30f6deec6bdb1ef92d708ab8.jpg",
+    "https://media.tate.org.uk/art/images/work/N/N01/N01522_10.jpg"
+];
+// Backgrounds
+books.backgrounds.name = [
+    "Smuggler's <br> Guild Member",
+    "Royalty",
+    "Doctor"
+];
+books.backgrounds.author = [
+    "DS",
+    "SM",
+    "SM"
+];
+books.backgrounds.url = [
+    "https://homebrewery.naturalcrit.com/share/zURAHso__Jb6",
+    "https://homebrewery.naturalcrit.com/share/975Hwi7oNUoy",
+    "https://homebrewery.naturalcrit.com/share/pEOKWgwFf4-R"
+];
+books.backgrounds.img = [
+    "https://cdnb.artstation.com/p/assets/images/images/014/260/095/large/igor-solovyev-3.jpg?1543236894",
+    "https://i.pinimg.com/750x/c4/8b/21/c48b21dea5c53d74c60ece42326d4afc.jpg",
+    "https://i.pinimg.com/564x/f7/1a/4b/f71a4bf19c61e6db2417645e7ec7da00.jpg"
+];
